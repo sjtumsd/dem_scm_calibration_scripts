fde42c8f879b64e5ee092789da9e162c33322351
Zym13430941761 <walt88@sjtu.edu.cn>
2024-07-27 15:27:27 +0800

    small

--- Demos/plt_demos/Singlewheel_Test.py
+++ Demos/plt_demos/Singlewheel_Test.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 from numpy import polynomial as P
 from scipy.interpolate import make_interp_spline
 from matplotlib import rcParams
-# ==============================================
-# Loop for two different wheels: HMMWV and VIPER
-# ==============================================
+# =====================================================
+# Loop for three different wheels: HMMWV, VIPER, Rover
+# =====================================================
 config = {
     "font.family":'Times New Roman',  # 设置字体类型
     "axes.unicode_minus": False #解决负号无法显示的问题
@@ -17,8 +17,7 @@ rcParams.update(config)
 for n_type in range(1):
     # Choose a wheel type
     wheel_type = "HMMWV_Wheel"
-    if n_type == 1:
-        wheel_type = "VIPER_Wheel"
+    
     print("Processing data for " + wheel_type)
 
     # General information of the data and plots
@@ -31,10 +30,7 @@ for n_type in range(1):
     xlim = [0, 15]
     ylim_dbp = [-200, 600]
     ylim_torque = [-20, 100]
-    if wheel_type == "VIPER_Wheel":
-        xlim = [0, 10]
-        ylim_dbp = [-100, 700]
-        ylim_torque = [-20, 280]
+    
     legend_pos = 'upper right'
     legend_ncol = 4
     FaceColor = 'w'
@@ -53,7 +49,7 @@ for n_type in range(1):
     #dir1 = "/research/sbel/d_chrono_fsi_granular/chrono_3001/chrono_build/bin/DEMO_OUTPUT/FSI_Single_Wheel_Test"
     #dir2_hmmwv = "/Regular_HMMWV_Tire/dx1cm/31-108kg-slip"
     #dir2_viper = "/Real_VIPER_Wheel/new_dx10mm_h10mm_height15cm/01-108kg-slip"
-    dir_singlewheel = "D:/HuaweiMoveData/Users/17908/Desktop/Data/Singlewheel/slip="
+    dir_singlewheel = "D:/dem_scm_calibration_scripts/Data collection/Singlewheel_Data/hmmwv_wheel_v=100_m=20/slip="
     #dir2 = dir2_hmmwv
     #if wheel_type == "VIPER_Wheel":
         #dir2 = dir2_viper
@@ -61,15 +57,9 @@ for n_type in range(1):
     slip_hmmwv = [["00", "01", "02", "03", "04", "05", "06", "07", "08"],
                   ["0.0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8"],
                  [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]]
-    #slip_hmmwv = [["00"],
-    #              ["0.0"],
-    #             [0.0]]
-    slip_viper = [["00", "003", "006", "01", "015", "02", "03", "04", "05", "06", "07", "08"],
-                  ["0.0", "0.03", "0.06", "0.1", "0.15", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8"],
-                  [0.0, 0.03, 0.06, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]]
+    
     slip = slip_hmmwv
-    if wheel_type == "VIPER_Wheel":
-        slip = slip_viper
+    
 
     dir3_dbp = "/data/DBP.txt"
     dir3_torque = "/data/Torque.txt"
@@ -84,31 +74,22 @@ for n_type in range(1):
     t_start = 5
 
     # Name of the plot and txt file
-    name_png_dbp_crm = "final_hmmwv_wheel_dbp_vs_time_crm.png"
-    name_png_torque_crm = "final_hmmwv_wheel_torque_vs_time_crm.png"
-    name_txt_dbp_torque_vs_slip_crm = "DBP_Torque_vs_Slip_HMMWV_Wheel_CRM.txt"
-    if wheel_type == "VIPER_Wheel":
-        name_png_dbp_crm = "final_viper_wheel_dbp_vs_time_crm.png"
-        name_png_torque_crm = "final_viper_wheel_torque_vs_time_crm.png"
-        name_txt_dbp_torque_vs_slip_crm = "DBP_Torque_vs_Slip_VIPER_Wheel_CRM.txt"
+    name_png_dbp_crm = "final_hmmwv_wheel_dbp_vs_time_dem.png"
+    name_png_torque_crm = "final_hmmwv_wheel_torque_vs_time_dem.png"
+    name_txt_dbp_torque_vs_slip_crm = "DBP_Torque_vs_Slip_HMMWV_Wheel_DEM.txt"
 
     # Name of the plot and txt file
     name_png_dbp_scm = "final_hmmwv_wheel_dbp_vs_time_scm.png"
     name_png_torque_scm = "final_hmmwv_wheel_torque_vs_time_scm.png"
     name_txt_dbp_torque_vs_time_scm = "DBP_Torque_vs_Time_HMMWV_Wheel_SCM.txt"
     name_txt_dbp_torque_vs_slip_scm = "DBP_Torque_vs_Slip_HMMWV_Wheel_SCM.txt"
-    if wheel_type == "VIPER_Wheel":
-        name_png_dbp_scm = "final_viper_wheel_dbp_vs_time_scm.png"
-        name_png_torque_scm = "final_viper_wheel_torque_vs_time_scm.png"
-        name_txt_dbp_torque_vs_time_scm = "DBP_Torque_vs_Time_VIPER_Wheel_SCM.txt"
-        name_txt_dbp_torque_vs_slip_scm = "DBP_Torque_vs_Slip_VIPER_Wheel_SCM.txt"
-
+    
     # ==============================================
-    # ==================== CRM =====================
+    # ==================== DEM =====================
     # ==============================================
     # ==============================================
     # ============= Plot DBP VS time ===============
-    print(" CRM: Plot DBP VS time")
+    print(" DEM: Plot DBP VS time")
     plt.figure(figsize = fig_size)
     font = {'weight': Weight, 'size': fs}
     plt.rc('font', **font)
@@ -162,7 +143,7 @@ for n_type in range(1):
 
     # ==============================================
     # ============ Plot Torque VS time =============
-    print(" CRM: Plot Torque VS time")
+    print(" DEM: Plot Torque VS time")
     plt.figure(figsize = fig_size)
     font = {'weight': Weight, 'size': fs}
     plt.rc('font', **font)
@@ -323,7 +304,7 @@ for n_type in range(1):
 
 
     # ==============================================
-    # ================= CRM VS SCM =================
+    # ================= DEM VS SCM =================
     # ==============================================
     # ==============================================
     # ============== DBP Torque vs Slip ============
@@ -341,27 +322,27 @@ for n_type in range(1):
     # Loop for DBP, Torque and Slope
     for k in range(3):
         if k == 0:
-            print(" CRM VS SCM: Plot DBP VS Slip")
+            print(" DEM VS SCM: Plot DBP VS Slip")
         if k == 1:
-            print(" CRM VS SCM: Plot Torque VS Slip")
+            print(" DEM VS SCM: Plot Torque VS Slip")
         if k == 2:
-            print(" CRM VS SCM: Plot Slope VS Slip")
+            print(" DEM VS SCM: Plot Slope VS Slip")
         plt.figure(figsize = fig_size)
         font = {'weight': Weight, 'size': fs}
         plt.rc('font', **font)
 
-        x_crm = DBP_Torque_Mean[0][:]
+        x_dem = DBP_Torque_Mean[0][:]
         x_scm = DBP_Torque_Mean_SCM[0][:]
 
-        y_crm = DBP_Torque_Mean[1][:]
+        y_dem = DBP_Torque_Mean[1][:]
         y_scm = DBP_Torque_Mean_SCM[1][:]
         if k == 1:
-            y_crm = DBP_Torque_Mean[2][:]
+            y_dem = DBP_Torque_Mean[2][:]
             y_scm = DBP_Torque_Mean_SCM[2][:]
         if k == 2: 
-            y_crm = 180 / math.pi * np.arctan(1.0 / tot_load * DBP_Torque_Mean[1][:])
+            y_dem = 180 / math.pi * np.arctan(1.0 / tot_load * DBP_Torque_Mean[1][:])
             y_scm = 180 / math.pi * np.arctan(1.0 / tot_load * DBP_Torque_Mean_SCM[1][:])
-        plt.plot(x_crm, y_crm, 'rs--', lw = lw, markersize = ms, label = "DEM")
+        plt.plot(x_dem, y_dem, 'rs--', lw = lw, markersize = ms, label = "DEM")
         plt.plot(x_scm, y_scm, 'b*-.', lw = lw, markersize = ms, label = "SCM")
 
         plt.grid(linestyle = '--')
@@ -380,14 +361,11 @@ for n_type in range(1):
         
         # Set xlim and ylim
         ax.set_xlim([-0.1, 0.9])
-        if n_type == 0 and k == 0:
+        if k == 0:
             ax.set_ylim([-30, 100])
-        if n_type == 0 and k == 1:
+        if k == 1:
             ax.set_ylim([-5, 55])
-        if n_type == 1 and k == 0:
-            ax.set_ylim([-100, 700])
-        if n_type == 1 and k == 1:
-            ax.set_ylim([0, 250])
+
 
         if k == 2:
             ax.set_ylim([-10, 30])
@@ -398,13 +376,11 @@ for n_type in range(1):
 
         # Set the output png name
         name_base = "final_hmmwv_wheel"
-        if n_type == 1:
-            name_base = "final_viper_wheel"
 
-        name = name_base + "_dbp_vs_slip_crm_vs_scm.png"
+        name = name_base + "_dbp_vs_slip_dem_vs_scm.png"
         if k == 1:
-            name = name_base + "_torque_vs_slip_crm_vs_scm.png"
+            name = name_base + "_torque_vs_slip_dem_vs_scm.png"
         if k == 2:
-            name = name_base + "_slope_vs_slip_crm_vs_scm.png"
+            name = name_base + "_slope_vs_slip_dem_vs_scm.png"
         plt.savefig(out_dir + name, facecolor = FaceColor, dpi = DPI)
         # plt.show()
